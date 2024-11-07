05ce416702aa1665d2e09ce6f3932b8b6f73a2fb
HyeonsangKim <k98hh@naver.com>
2024-11-07 20:38:47 +0900

    setting change for vercel

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "./app/utils/supabase/middleware";
 
+export const runtime = "nodejs";
+
 export async function middleware(request: NextRequest) {
   // 서버 액션과 정적 리소스는 미들웨어를 건너뛰도록 함
   if (
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Node.js 런타임 사용을 명시
+  experimental: {
+    serverActions: true,
+  },
+  runtime: "nodejs",
+  // 나머지 설정 유지
   output: "standalone",
   images: {
     domains: ["rymesndbhkasxusysszb.supabase.co"],
