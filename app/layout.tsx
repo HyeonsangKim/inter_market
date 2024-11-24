aa476eb617620394bc8b2891b4e987893d6f5263
HyeonsangKim <k98hh@naver.com>
2024-11-24 17:13:40 +0900

    no dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   title: "Interagora",
   description: "Inter Agora",
 };
-export const dynamic = "force-dynamic";
+
 export default function RootLayout({
   children,
 }: Readonly<{
